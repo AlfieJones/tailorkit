db93dc78dfdb75814bcd72898b2e821b58256801
Alfie Jones <alfie.jones@hotmail.co.uk>
2026-04-25 22:07:40 +1000

    initial commit

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,50 @@
+import { polar, checkout, portal } from "@polar-sh/better-auth";
+import { createDb } from "@tailorkit-new/db";
+import * as schema from "@tailorkit-new/db/schema/auth";
+import { env } from "@tailorkit-new/env/server";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { tanstackStartCookies } from "better-auth/tanstack-start";
+
+import { polarClient } from "./lib/payments";
+
+export function createAuth() {
+  const db = createDb();
+
+  return betterAuth({
+    database: drizzleAdapter(db, {
+      provider: "pg",
+
+      schema: schema,
+    }),
+    trustedOrigins: [env.CORS_ORIGIN],
+    emailAndPassword: {
+      enabled: true,
+    },
+    secret: env.BETTER_AUTH_SECRET,
+    baseURL: env.BETTER_AUTH_URL,
+    plugins: [
+      polar({
+        client: polarClient,
+        createCustomerOnSignUp: true,
+        enableCustomerPortal: true,
+        use: [
+          checkout({
+            products: [
+              {
+                productId: "your-product-id",
+                slug: "pro",
+              },
+            ],
+            successUrl: env.POLAR_SUCCESS_URL,
+            authenticatedUsersOnly: true,
+          }),
+          portal(),
+        ],
+      }),
+      tanstackStartCookies(),
+    ],
+  });
+}
+
+export const auth = createAuth();
---
+++ packages/auth/src/lib/payments.ts
@@ -0,0 +1,7 @@
+import { Polar } from "@polar-sh/sdk";
+import { env } from "@tailorkit-new/env/server";
+
+export const polarClient = new Polar({
+  accessToken: env.POLAR_ACCESS_TOKEN,
+  server: "sandbox",
+});
