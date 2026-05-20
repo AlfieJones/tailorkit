b03229559b9660cf907c3f8ba174b7b271d2c982
Alfie Jones <alfie.jones@hotmail.co.uk>
2026-05-20 17:05:26 +1000

    adding nitro

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -2,12 +2,14 @@ import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact, { reactCompilerPreset } from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { nitro } from "nitro/vite";
 import babel from "@rolldown/plugin-babel";
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
-    tanstackStart({}),
+    tanstackStart(),
+    nitro(),
     viteReact(),
     babel({
       presets: [reactCompilerPreset()],
