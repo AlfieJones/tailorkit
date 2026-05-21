62eea2b985ba4f8cbd82fdba87992245a3bc225d
Alfie Jones <alfie.jones@hotmail.co.uk>
2026-05-21 12:58:51 +1000

    prefixing server

--- apps/docs/vite.config.ts
+++ apps/docs/vite.config.ts
@@ -18,6 +18,9 @@ export default defineConfig({
       prerender: {
         enabled: true,
       },
+      serverFns: {
+        base: "/docs/_serverFn",
+      },
     }),
     react(),
     nitro(),
